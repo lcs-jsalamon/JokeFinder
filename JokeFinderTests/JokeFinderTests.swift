83f3cad884895ec259955fe3496bd604d5b1863e
Josephine Salamon <jsalamon@lcs.on.ca>
2025-03-03 14:44:10 -0500

    Initial Commit

---
+++ JokeFinderTests/JokeFinderTests.swift
@@ -0,0 +1,17 @@
+//
+//  JokeFinderTests.swift
+//  JokeFinderTests
+//
+//  Created by Josephine Salamon on 2025-03-03.
+//
+
+import Testing
+@testable import JokeFinder
+
+struct JokeFinderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
